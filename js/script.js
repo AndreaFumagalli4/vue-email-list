20ddc2a36fe96adf44d14b696cb29fe5b7d2eb78
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-12-21 15:13:23 +0100

    create and print a random email

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            newMail : '',
         }
+    },
+    methods : {
+        getNewMail () {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data.response);
+                this.newMail = response.data.response;
+            })
+            .catch((error) => {
+                console.warn(error);
+            })
+        }
+    },
+
+    created() {
+        this.getNewMail();
     }
 }).mount('#app');
